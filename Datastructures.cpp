4081c125af4d89fabcbada6320aaae7d43353095
ArafatAljabry <269424@stud.inn.no>
2024-12-16 16:13:48 +0100

    finished lnked list

--- Datastructures.cpp
+++ Datastructures.cpp
@@ -59,7 +59,7 @@ public:
 	{
 		m_Head = nullptr;
 	}
-private:
+public:
 	Node* m_Head;
 
 	void AddToFront(int value)
@@ -70,7 +70,12 @@ private:
 
 	void AddToBack(int value)
 	{
+		// If used while the list is empty,adds to the front.(Which is the same, since its empty)
+		if (m_Head == nullptr) { AddToFront(value); return; }
+
 		Node* CurrentLastValue = m_Head;
+
+		//Get the last node
 		while(CurrentLastValue->GetNextNode() != nullptr)
 		{
 			CurrentLastValue = CurrentLastValue->GetNextNode();
@@ -83,7 +88,7 @@ private:
 	void DeleteFromBack()
 	{
 		Node* tempNode;
-		Node* SecondToLast;
+		Node* SecondToLast = m_Head;
 
 		//Get the second to last node
 		while(SecondToLast->GetNextNode()->GetNextNode() !=nullptr)
@@ -117,14 +122,44 @@ private:
 			{
 				m_Head = nullptr;
 				delete CurrentHead;
+				CurrentHead = nullptr;
 			}
 		}
-		else { std::cout << 'Linked list is empty'; }
+		else { std::cout << "Linked list is empty"; }
 			
+	}
+
+	void PrintNodes()
+	{
+		Node* tempNode = m_Head;
+
+		while (tempNode != nullptr)
+		{
+			std::cout << tempNode->GetValue();
+			tempNode = tempNode->GetNextNode();
+		}
 	}
 };
 int main()
 {
+	LinkedList myLinkedList;
+
+
+	myLinkedList.AddToFront(1);
+	myLinkedList.AddToBack(2);
+	myLinkedList.AddToBack(3);
+	myLinkedList.AddToBack(4);
+	myLinkedList.PrintNodes();
+
+
+	myLinkedList.DeleteFromFront();
+	std::cout << "\nAfter deleting from front\n";
+	myLinkedList.PrintNodes();
+
+	myLinkedList.DeleteFromBack();
+	std::cout << "\nAfter deleting from back\n";
+	myLinkedList.PrintNodes();
+	
 
 	return 0;
 }
